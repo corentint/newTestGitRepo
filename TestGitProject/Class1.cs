6e48fe799b4ff58d1b70a3498bc4f235373447c7
Corentin Thevenet <corentin.thevenet.efrei@gmail.com>
2014-02-14 00:35:39 +0100

    Modification to Class1

--- TestGitProject/Class1.cs
+++ TestGitProject/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +7,6 @@ namespace TestGitProject
 {
     class Class1
     {
+		// My brand new class!
     }
 }
